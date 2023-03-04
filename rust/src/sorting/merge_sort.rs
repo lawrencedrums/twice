378634944085a98141585dad93f2f799f90fd657
Lawrence Wong <lawrence.wmc@pm.me>
2023-03-04 18:17:37 +0000

    feat: bottom up merge sort with tests

--- rust/src/sorting/merge_sort.rs
+++ rust/src/sorting/merge_sort.rs
@@ -1,3 +1,6 @@
+use std::cmp::min;
+
+
 pub fn top_down_merge_sort<T: Ord + Copy>(nums: &mut [T]) {
 	if nums.len() <= 1 {
 		return;
@@ -9,7 +12,27 @@ pub fn top_down_merge_sort<T: Ord + Copy>(nums: &mut [T]) {
 	merge(nums, mid);
 }
 
-pub fn bottom_up_merge_sort<T: Ord + Copy>(nums: &mut [T]) {}
+pub fn bottom_up_merge_sort<T: Ord + Copy>(nums: &mut [T]) {
+	if nums.len() <= 1 {
+		return;
+	}
+
+	let len = nums.len();
+	let high_idx = len - 1;
+	let mut subarray_len = 1;
+
+	while subarray_len < len {
+		let mut start = 0;
+		while subarray_len < len - start {
+			let end = min(start + 2*subarray_len, len);
+
+			merge(&mut nums[start..end], subarray_len);
+
+			start = end;
+		}
+		subarray_len *= 2;
+	}
+}
 
 fn merge<T: Ord + Copy>(nums: &mut [T], mid: usize) {
 	let left_half = nums[..mid].to_vec();
@@ -33,24 +56,47 @@ fn merge<T: Ord + Copy>(nums: &mut [T], mid: usize) {
 mod tests {
     use super::*;
 
+    // top down (recursive) test cases
     #[test]
-    fn test_sort() {
-        let mut nums = vec![3, 2, 4, 1, 5];
+    fn test_top_down_merge_sort() {
+        let mut nums = vec![7, 6, 3, 9, 2, 4, 1, 8, 10, 5];
         top_down_merge_sort(&mut nums);
-        assert_eq!(nums, vec![1, 2, 3, 4, 5]);
+        assert_eq!(nums, vec![1, 2, 3, 4, 5, 6, 7, 8, 9 ,10]);
     }
 
     #[test]
-    fn test_sort_empty_array() {
+    fn test_top_down_merge_sort_empty_array() {
         let mut nums = Vec::<i32>::new();
         top_down_merge_sort(&mut nums);
         assert_eq!(nums, vec![]);
     }
 
     #[test]
-    fn test_sort_one_element() {
+    fn test_top_down_merge_sort_one_element() {
         let mut nums = vec![1];
         top_down_merge_sort(&mut nums);
+        assert_eq!(nums, vec![1]);
+    }
+
+    // bottom up (iterative) test cases
+    #[test]
+    fn test_bottom_up_merge_sort() {
+        let mut nums = vec![7, 6, 3, 9, 2, 4, 1, 8, 10, 5];
+        bottom_up_merge_sort(&mut nums);
+        assert_eq!(nums, vec![1, 2, 3, 4, 5, 6, 7, 8, 9 ,10]);
+    }
+
+    #[test]
+    fn test_bottom_up_merge_sort_empty_array() {
+        let mut nums = Vec::<i32>::new();
+        bottom_up_merge_sort(&mut nums);
+        assert_eq!(nums, vec![]);
+    }
+
+    #[test]
+    fn test_bottom_up_merge_sort_one_element() {
+        let mut nums = vec![1];
+        bottom_up_merge_sort(&mut nums);
         assert_eq!(nums, vec![1]);
     }
 }
