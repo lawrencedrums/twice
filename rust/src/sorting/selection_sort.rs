947b202ae0c9ce6472fcc1e55a7a8a5ffd1f9d94
Lawrence Wong <lawrence.wmc@pm.me>
2023-03-04 14:58:19 +0000

    test: added tests to selection sort

--- rust/src/sorting/selection_sort.rs
+++ rust/src/sorting/selection_sort.rs
@@ -1,4 +1,8 @@
 pub fn selection_sort(nums: &mut Vec<i32>) {
+    if nums.is_empty() {
+        return;
+    }
+
     for i in 0..nums.len() {
         let mut min = i;
         let mut j = i + 1;
@@ -13,5 +17,31 @@ pub fn selection_sort(nums: &mut Vec<i32>) {
         if min != i {
             nums.swap(min, i);
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sort() {
+        let mut nums = vec![3, 2, 4, 1, 5];
+        selection_sort(&mut nums);
+        assert_eq!(nums, vec![1, 2, 3, 4, 5]);
+    }
+
+    #[test]
+    fn test_sort_empty_array() {
+        let mut nums = Vec::<i32>::new();
+        selection_sort(&mut nums);
+        assert_eq!(nums, vec![]);
+    }
+
+    #[test]
+    fn test_sort_one_element() {
+        let mut nums = vec![1];
+        selection_sort(&mut nums);
+        assert_eq!(nums, vec![1]);
     }
 }
