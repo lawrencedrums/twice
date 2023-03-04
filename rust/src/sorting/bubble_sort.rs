1543d941e6014f314d7d9c1254c23d31976a28ea
Lawrence Wong <lawrence.wmc@pm.me>
2023-03-04 14:04:33 +0000

    Added tests to bubble sort

--- rust/src/sorting/bubble_sort.rs
+++ rust/src/sorting/bubble_sort.rs
@@ -13,3 +13,27 @@ pub fn bubble_sort(nums: &mut Vec<i32>) {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sort() {
+        let mut nums = vec![3, 5, 1, 9, 6];
+        bubble_sort(&mut nums);
+        assert_eq!(nums, vec![1, 3, 5, 6, 9]);
+    }
+
+    fn test_sort_empty_array() {
+        let mut nums = Vec::<i32>::new();
+        bubble_sort(&mut nums);
+        assert_eq!(nums, vec![]);
+    }
+
+    fn test_sort_one_element() {
+        let mut nums = vec![1];
+        bubble_sort(&mut nums);
+        assert_eq!(nums, vec![1]);
+    }
+}
