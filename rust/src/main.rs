73782ca29486fe431efd48ba27d911c8e684dc42
Lawrence Wong <lawrence.wmc@pm.me>
2023-03-07 23:56:46 +0000

    feat: show time took to sort

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,20 +1,22 @@
 use rand::seq::SliceRandom;
 use rand::thread_rng;
+use std::time::{Duration, Instant};
 
 mod sorting;
 
 
 fn main() {
-    let size = 200;
+    let size = 500;
+    let sorted: Vec<i32> = (0..size).collect();
+    let mut nums: Vec<i32> = (0..size).collect();
 
     let mut rng = thread_rng();
-    let mut nums: Vec<i32> = (0..size).collect();
-    let sorted: Vec<i32> = (0..size).collect();
-
     nums.shuffle(&mut rng);
-    sorting::timsort::timsort(&mut nums);
 
-    // println!("{nums:?}");
+    let start = Instant::now();
+    sorting::quicksort::quicksort(&mut nums);
+    let duration = start.elapsed();
+
     assert_eq!(nums, sorted);
-    println!("sorted!")
+    println!("Sorting took {duration:?}.");
 }
